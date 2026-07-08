8bbf43a01ff7c8ecdbfa6e3efe594d795599c655
Alessandro Cídney <contatoalessandrocidneysa@gmail.com>
2026-07-07 23:36:25 -0300

    fix: Add return to conditional

--- Java/9-pacotes/caixadeferramentas/manuais/ChaveDeFenda.java
+++ Java/9-pacotes/caixadeferramentas/manuais/ChaveDeFenda.java
@@ -10,6 +10,7 @@ public class ChaveDeFenda {
   public void usar() {
     if (estado == "enferrujado") {
       System.out.println(this.getClass().getSimpleName() + " está enferrujado! Não vai dar pra usar...");
+      return;
     }
 
     System.out.println("apertando...");
