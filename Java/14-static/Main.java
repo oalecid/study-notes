94dcc0c79bad20919c70a94d77b29579c5d71835
Alessandro Cídney <contatoalessandrocidneysa@gmail.com>
2026-07-15 00:06:23 -0300

    feat: Added notes related to "static"

---
+++ Java/14-static/Main.java
@@ -0,0 +1,7 @@
+import operacoes.*;
+
+public class Main {
+  public static void main(String args[]) {
+    System.out.println("A soma entre 1 e 2 é " + Calculadora.soma(1, 2));
+  }
+}
---
+++ Java/14-static/operacoes/Calculadora.java
@@ -0,0 +1,9 @@
+package operacoes;
+
+// static permite que o item seja utilizado mesmo que a classe não esteja instanciada
+
+public class Calculadora {
+  public static int soma(int a, int b) {
+    return a + b;
+  }
+}
