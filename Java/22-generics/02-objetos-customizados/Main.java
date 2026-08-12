fc4272bd3fd4a2afe31d29e5c7e554907efc1fca
Alessandro Cídney <contatoalessandrocidneysa@gmail.com>
2026-08-11 22:25:45 -0300

    feat: Update notes about generics

---
+++ Java/22-generics/02-objetos-customizados/Main.java
@@ -0,0 +1,83 @@
+import java.util.List;
+import java.util.ArrayList;
+
+// Algumas classes para os exemplos
+class Pessoa {
+  private String nome;
+  private String sobrenome;
+
+  Pessoa(String nomeInit, String sobrenomeInit) {
+    nome = nomeInit;
+    sobrenome = sobrenomeInit;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public String getSobrenome() {
+    return sobrenome;
+  }
+}
+
+class Guerreiro extends Pessoa {
+  Guerreiro(String nomeInit, String sobrenomeInit) {
+    super(nomeInit, sobrenomeInit);
+  }
+
+  void atacar() {
+    System.out.println("ataaaaacaaaaarrr!!!");
+  }
+}
+
+class Ferreiro extends Pessoa {
+  Ferreiro(String nomeInit, String sobrenomeInit) {
+    super(nomeInit, sobrenomeInit);
+  }
+
+  void minerar() {
+    System.out.println("mineeeeraaaaaarrrr!!!");
+  }
+}
+
+// O exibidor de nomes consegue manipular independentemente do tipo que receber
+// com tanto que o tipo seja descendente "Pessoa"
+class ListaDePessoas<T extends Pessoa> {
+  private List<T> lista = new ArrayList<>();
+  
+  public void adicionarPessoa(T novaPessoa) {
+    lista.add(novaPessoa);
+  }
+
+  public T obterPessoa(int indice) {
+    // Sem o generic (usando diretamente a superclasse), ao pegar o valor via obterPessoa(),
+    // não seria possível usar seus métodos customizados, como o atacar() e minerar()
+
+    return lista.get(indice);
+  }
+
+  public void exibirNomeCompleto(int indice) {
+    T pessoaAtual = lista.get(indice);
+
+    System.out.println(String.format("%s %s", pessoaAtual.getNome(), pessoaAtual.getSobrenome()));
+  }
+}
+
+public class Main {
+  public static void main(String[] args) {
+    ListaDePessoas<Guerreiro> listaDeGuerreiros = new ListaDePessoas<>();
+    
+    listaDeGuerreiros.adicionarPessoa(new Guerreiro("Guerreiro", "Fulano"));
+    
+    listaDeGuerreiros.exibirNomeCompleto(0);
+    listaDeGuerreiros.obterPessoa(0).atacar(); // <- o generic permite que usemos atacar()
+
+
+    ListaDePessoas<Ferreiro> listaDeFerreiros = new ListaDePessoas<>();
+
+    listaDeFerreiros.adicionarPessoa(new Ferreiro("Ferreiro", "Cicrano"));
+    
+    listaDeFerreiros.exibirNomeCompleto(0);
+    listaDeFerreiros.obterPessoa(0).minerar();
+  }
+}
