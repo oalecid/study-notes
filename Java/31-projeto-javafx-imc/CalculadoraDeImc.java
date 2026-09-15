6640b6e1b01baf45ae47dbea977119df682ef991
Alessandro Cídney <contatoalessandrocidneysa@gmail.com>
2026-09-15 02:52:13 -0300

    feat: Create BMI calculator project

--- Java/31-projeto-javafx-imc/CalculadoraDeImc.java
+++ Java/31-projeto-javafx-imc/CalculadoraDeImc.java
@@ -14,22 +14,35 @@ public class CalculadoraDeImc extends Application {
   @Override
   public void start(Stage palcoPrincipal) {
     Label labelDoPeso = new Label("Informe o peso:");
+    labelDoPeso.getStyleClass().add("form-label");
+
     TextField campoDePeso = new TextField();
+    campoDePeso.setPromptText("Peso em kg"); // Placeholder
+    campoDePeso.getStyleClass().add("form-text-field");
 
-    Label labelDaAltura = new Label("Informe a altura");
+    Label labelDaAltura = new Label("Informe a altura:");
+    labelDaAltura.getStyleClass().add("form-label");
+
     TextField campoDaAltura = new TextField();
+    campoDaAltura.setPromptText("Altura em metros"); // Placeholder
+    campoDaAltura.getStyleClass().add("form-text-field");
 
-    Button botaoDeCalcular = new Button("Calcular");
+    Button botaoDeCalcular = new Button("Calcular IMC");
+    botaoDeCalcular.getStyleClass().add("form-button");
 
-    Label labelDoResultado = new Label("");
+    Label labelDoResultado = new Label();
+    labelDoResultado.getStyleClass().add("form-result");
 
     botaoDeCalcular.setOnAction(e -> {
       pegarValoresECalcularIMC(campoDePeso, campoDaAltura, labelDoResultado);
     });
 
     VBox vBox = new VBox(labelDoPeso, campoDePeso, labelDaAltura, campoDaAltura, botaoDeCalcular, labelDoResultado);
+    vBox.getStyleClass().add("form-box");
 
     Scene cena = new Scene(vBox, 500, 500);
+
+    cena.getStylesheets().add("file:assets/styles.css");
 
     palcoPrincipal.setTitle("Calculadora de IMC");
     palcoPrincipal.setScene(cena);
@@ -39,13 +52,20 @@ public class CalculadoraDeImc extends Application {
 
   void pegarValoresECalcularIMC(TextField campoDePeso, TextField campoDeAltura, Label labelDoResultado) {
     try {
+      String pesoEmString = campoDePeso.getText();
+      String alturaEmString = campoDeAltura.getText();
+
+      if (pesoEmString.isEmpty() || alturaEmString.isEmpty()) {
+        throw new IllegalArgumentException("Preencha todos os campos antes de continuar!");
+      }
+
       double peso = converterStringParaFloat(campoDePeso.getText());
       
       double altura = converterStringParaFloat(campoDeAltura.getText());
 
       double resultado = IMC.calcular(peso, altura);
 
-      labelDoResultado.setText("Resultado: " + resultado);
+      labelDoResultado.setText(String.format("O IMC para %.2f kg e %.2f metros é %.2f", peso, altura, resultado));
     } catch (Exception e) {
       labelDoResultado.setText("Ocorreu um erro: " + e.getMessage());
     }
@@ -53,7 +73,13 @@ public class CalculadoraDeImc extends Application {
 
   double converterStringParaFloat(String valorEmString) {
     try {
-      return Double.parseDouble(valorEmString);
+      String valorFormatado = valorEmString;
+
+      if (valorEmString.contains(",")) {
+        valorFormatado = valorEmString.replace(",", ".");
+      }
+
+      return Double.parseDouble(valorFormatado);
     } catch (NumberFormatException e) {
       throw new IllegalArgumentException("Valores inválidos!");
     }
